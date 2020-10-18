0b027e5b41f49debebd2509dba3e7bf9d53eb8fe
pranak <pranakbarua111@gmail.com>
2020-10-18 10:38:18 +0600

    root

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(cors())
 
 const port = 5000
 
-
+app.get('/',(req,res)=>{
+  res.send('working')
+})
 const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("emaJohnStore").collection("products");
